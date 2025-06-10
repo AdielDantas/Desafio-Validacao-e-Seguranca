279957ed4c0de8b7693941160c248a3f743ea1b9
Adiel Dantas <adieldantasn@gmail.com>
2025-06-10 13:34:09 -0300

    Event CRUD

---
+++ src/main/java/com/devsuperior/bds04/service/EventService.java
@@ -0,0 +1,37 @@
+package com.devsuperior.bds04.service;
+
+import com.devsuperior.bds04.dto.EventDTO;
+import com.devsuperior.bds04.entities.Event;
+import com.devsuperior.bds04.repositories.EventRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class EventService {
+
+    @Autowired
+    private EventRepository repository;
+
+    @Transactional(readOnly = true)
+    public Page<EventDTO> findAll(Pageable pageable) {
+        Page<Event> events = repository.findAll(pageable);
+        return events.map(x -> new EventDTO(x));
+    }
+
+    @Transactional
+    public EventDTO insert (EventDTO dto) {
+        Event event = new Event();
+        copyDtoToEntity(dto, event);
+        event = repository.save(event);
+        return new EventDTO(event);
+    }
+
+    private void copyDtoToEntity(EventDTO dto, Event event) {
+        event.setName(dto.getName());
+        event.setDate(dto.getDate());
+        event.setUrl(dto.getUrl());
+    }
+}
